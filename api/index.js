90494b3660a317ffa3b985898cb12730771de95d
AR-Script <ar5.personalmail@gmail.com>
2024-01-05 19:27:13 +0530

    Connected to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//to connect to MongoDB
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log('Connected to MongoDB!');  //to check if it connection to MongoDB is successful
+    }).catch(() => {
+        console.log(err); //to catch the error if connection to MongoDB is unsuccessful
+    })
 
 const app = express();
 
