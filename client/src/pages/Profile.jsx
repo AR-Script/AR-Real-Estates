a93eff77842a1de0ce495a1594c78adb71c5b4f8
AR-Script <ar5.personalmail@gmail.com>
2024-01-23 21:04:33 +0530

    Completion of delete User Listings functionality within the Profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -128,6 +128,22 @@ export default function Profile() {
       }
     };
 
+    const handleListingDelete = async (listingId) => {
+      try {
+        const res = await fetch(`/api/listing/delete/${listingId}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if(data.success === false) {
+          console.log(data.message);
+          return;
+        }
+        setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -178,7 +194,7 @@ export default function Profile() {
           </Link>
           <div className="flex flex-col gap-3 items-center"> 
             <button className="text-slate-700 uppercase border border-slate-700 rounded hover:shadow-lg p-1 w-20">Edit</button>
-            <button className="text-red-700 uppercase border border-red-700 rounded hover:shadow-lg p-1 w-20">Delete</button>
+            <button onClick={()=>handleListingDelete(listing._id)} className="text-red-700 uppercase border border-red-700 rounded hover:shadow-lg p-1 w-20">Delete</button>
           </div>
         </div>
       ))
